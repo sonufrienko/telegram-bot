ec3c0b2a4f34907167f517b11485f9c5ca9c339f
Sergey Onufrienko <sergey@onufrienko.com>
2019-03-24 00:15:41 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const TelegramBot = require("node-telegram-bot-api");
+const AWS = require("aws-sdk");
+
+const { TELEGRAM_TOKEN, SQS_URL, AWS_REGION } = process.env;
+AWS.config.update({ region: AWS_REGION });
+const sqs = new AWS.SQS({ apiVersion: "2012-11-05" });
+const stat = util.promisify(sqs.sendMessage);
+
+const bot = new TelegramBot(TELEGRAM_TOKEN, {
+  polling: true
+});
+
+const addMessageToSQS = async msg => {
+  const {
+    from: { first_name, last_name },
+    text
+  } = msg;
+  console.log(`${first_name} ${last_name} >> ${text}`);
+
+  const jobParams = {
+    MessageBody: JSON.stringify(msg),
+    QueueUrl: SQS_URL
+  };
+
+  sqs.sendMessage(jobParams, (err, data) => {
+    if (err) return console.log(err);
+
+    bot.sendMessage(msg.chat.id, "Ok");
+  });
+};
+
+bot.on("message", addMessageToSQS);
